0a086ef2a0d18f8f93ad12f92e7883fbb859cb76
andres <andresdavid.ortega@gmail.com>
2025-01-09 11:32:58 -0500

    se agrego update_quantity

--- shop_app/views.py
+++ shop_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from .models import Product,Cart,CartItem
 from .serializers import ProductSerializer,DetailedProductSerializer,CartItemSerializer,SimpleCartSerializer,CartSerializer
 from rest_framework.response import Response
-
+from core.models import CustomUser
 
 
 @api_view(['GET'])
@@ -25,7 +25,9 @@ def add_item(request):
         cart_code=request.data.get('cart_code')
         product_id=request.data.get('product_id')
         
-        cart,created=Cart.objects.get_or_create(cart_code=cart_code,user=1) #QUITAR USER ID CUANDO SE HAGA EL LOGIN
+        user=CustomUser.objects.get(id=1)
+        cart,created=Cart.objects.get_or_create(cart_code=cart_code,user=user) #QUITAR USER ID CUANDO SE HAGA EL LOGIN
+        print(created)
         product=Product.objects.get(id=product_id)
         
         cartitem,created=CartItem.objects.get_or_create(cart=cart,product=product)
@@ -43,11 +45,16 @@ def add_item(request):
 def product_in_cart(request):
     cart_code=request.query_params.get('cart_code')
     product_id=request.query_params.get('product_id')
+    try:
+        cart=Cart.objects.get(cart_code=cart_code)
+        product=Product.objects.get(id=product_id)
+        product_exists_in_cart=CartItem.objects.filter(cart=cart, product=product).exists()
+    except:
+        product_exists_in_cart=False
     
-    cart=Cart.objects.get(cart_code=cart_code)
-    product=Product.objects.get(id=product_id)
     
-    product_exists_in_cart=CartItem.objects.filter(cart=cart, product=product).exists()
+    
+   
     
     return Response({'product_in_cart':product_exists_in_cart})    
     
@@ -66,3 +73,17 @@ def get_cart(request):
     serializer=CartSerializer(cart)
     return Response(serializer.data)
 
+@api_view(['PATCH'])
+def update_quantity(request):
+    try:
+        cartitem_id= request.data.get('item_id')
+        quantity=request.data.get('quantity')
+        quantity=int(quantity)
+        cartitem= CartItem.objects.get(id=cartitem_id)
+        cartitem.quantity=quantity
+        cartitem.save()
+        serializer=CartItemSerializer(cartitem)
+        return Response({'data':serializer.data, 'message':'Cartitem updated successfully'})
+    
+    except Exception as e:
+        return Response({'error':str(e)},status=400)
